d7571625cd2d03dab1e68ad29010e79c626d165d
Subhasish <subhasish469@gmail.com>
2020-05-20 10:50:47 +0530

    commit 3

--- app/src/main/java/com/example/audiovideo/MainActivity.java
+++ app/src/main/java/com/example/audiovideo/MainActivity.java
@@ -14,8 +14,10 @@ import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.net.URI;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{ //mainactivity implements the methods of onClickListener so this is also the method of setting onClickListener
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,MediaPlayer.OnCompletionListener{ //mainactivity implements the methods of onClickListener so this is also the method of setting onClickListener
 
     private VideoView myVideoView;
     private Button playBtn,btnPlayMusic,btnPauseMusic;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private MediaPlayer mediaPlayer;
     private SeekBar seekBarVolume;
     private SeekBar seekBarMove;
+    private Timer timer;
 
 
 
@@ -84,19 +87,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if(fromUser==true){
                     //Toast.makeText(MainActivity.this,progress+"",Toast.LENGTH_SHORT).show();
+                    mediaPlayer.seekTo(progress);
                 }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
+                mediaPlayer.pause();  //when user interacts with the seekbar
+
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+                mediaPlayer.start();  //when user stops interacting with seek bar
+
             }
         });
+        seekBarMove.setMax(mediaPlayer.getDuration());
+        mediaPlayer.setOnCompletionListener(this);
+
+
 
     }
 
@@ -115,11 +127,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.btnPlayMusic:
                 mediaPlayer.start();
+                //creating a thread here for the seek
+                timer=new Timer();          //timer is a thread that will operate behind the main UI
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        seekBarMove.setProgress(mediaPlayer.getCurrentPosition());  //mediaPlayer allows the music to play and gets the current position of the user
+
+                    }
+                },0,1000);  //delay =0 because as soon as the button is tapped start the heavy operation behind not on the UI directly in contact with the user as it will slow down the other components of the user UI and timer gets updated every 1 sec
+
 
                 break;
             case R.id.btnPauseMusic:
 
                 mediaPlayer.pause();
+                timer.cancel();  //when the pause is pressed the timer must be cancelled else the thread will consume more resources
+
 
                 break;
 
@@ -128,6 +152,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+        timer.cancel();
+        Toast.makeText(getApplicationContext(),"Music ended",Toast.LENGTH_SHORT).show();
 
     }
 }
